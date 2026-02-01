4fc623b1dc96f2211986e61a66f2cd2725af9e3d
carlosmartinsgomes <carlosmartins.gomes@hotmail.com>
2026-02-01 15:33:29 +0000

    Update wayback_spo_analyzer.py

--- wayback_spo_analyzer.py
+++ wayback_spo_analyzer.py
@@ -383,11 +383,11 @@ def parse_args():
     p.add_argument("--log-file", default=None)
     p.add_argument("--out", required=True)
 
-    p.add_argument("--start-year", type=int, default=2025)
-    p.add_argument("--start-month", type=int, default=11)
+    p.add_argument("--start-year", type=int, default=2024)
+    p.add_argument("--start-month", type=int, default=1)
     
-    p.add_argument("--end-year", type=int, default=2026)
-    p.add_argument("--end-month", type=int, default=1)
+    p.add_argument("--end-year", type=int, default=2024)
+    p.add_argument("--end-month", type=int, default=4)
 
 
     return p.parse_args()
